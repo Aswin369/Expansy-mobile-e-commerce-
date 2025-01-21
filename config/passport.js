e677e007b37dccceee8e3cbb2a86277b8b0ccd55
Aswin369 <aswinv793@gmail.com>
2025-01-21 18:05:22 +0530

    google auth completely working fine

--- config/passport.js
+++ config/passport.js
@@ -4,41 +4,49 @@ const User = require("../models/userSchema")
 const env = require("dotenv").config()
 
 
-passport.use(new GoogleStrategy({
-    clientID:process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
-},
-    async (accessToken,refreshToken, profile, done)=>{
-        try {
-            console.log("akudfhalsdkhfsd")
-            let user = await User.findOne({googleId:profile.id})
-            if(user){
-                return done(null, user)
-            }else{
-                user = new User({
-                    name:profile.displayName,
-                    email:profile.email[0].value,
-                    googleId:profile.id,
-                })
-                await user.save()
-                return done(null, user)
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "/auth/google/callback",
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            try {
+                // console.log("Google Profile:", profile);
+                const email = profile.emails && profile.emails[0] && profile.emails[0].value;
+
+                if (!email) {
+                    return done(new Error("No email found in Google profile"), null);
+                }
+
+                let user = await User.findOne({ googleId: profile.id });
+                if (user) {
+                    return done(null, user);
+                } else {
+                    user = new User({
+                        name: profile.displayName,
+                        email: email,
+                        googleId: profile.id,
+                    });
+                    // console.log("New User:", user);
+                    await user.save();
+                    return done(null, user);
+                }
+            } catch (error) {
+                return done(error, null);
             }
-        } catch (error) {
-            return done(error, null)
         }
-    }
-))
+    )
+);
 
 // Using assgin user deatials to session
 passport.serializeUser((user,done)=>{
-    console.log("akudfhalsdkhfsd")
     done(null, user.id)
 })
 
 // Using for fetch user data from session
 passport.deserializeUser((id,done)=>{
-    console.log("akudfhalsdkhfsd")
     User.findById(id)
     .then((user)=>{
         done(null, user)
