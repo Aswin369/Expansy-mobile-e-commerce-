30cde07304032cd60d895b2bc24ade2d71e204fc
Aswin369 <aswinv793@gmail.com>
2025-02-05 12:12:05 +0530

    variant management completed

--- controllers/admin/variantController.js
+++ controllers/admin/variantController.js
@@ -4,7 +4,13 @@ const Variant = require("../../models/variantSchema")
 
 const getVariantList = async (req,res)=>{
     try {
-        cosnt variantData = await 
+        const ramData = await Variant.find({category:"Ram"})
+        const StorageData = await Variant.find({category:"Storage"})
+        res.render("variant",{
+            ramData,
+            StorageData
+        })
+        // console.log(StorageData)
     } catch (error) {
         console.error("Error in gettingVariant Page",error)
         res.redirect("/pageerror")
@@ -37,9 +43,83 @@ const addVariants = async (req,res)=>{
         console.log("1")
         await newVariant.save()
         return res.json({message: "Variant is added successfully"})
-        console.log("2 addedd")
+        
     } catch (error) {
         console.error("Error in add variant",error)
+        res.redirect("/pageerror")
+    }
+}
+
+const blockRam = async (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log("This is my blockRam", id);
+        await Variant.findByIdAndUpdate(id,{$set:{isBlocked:true}});
+        res.redirect("/admin/getVariant");
+    } catch (error) {
+        console.error("Error blocking RAM:", error);
+        res.redirect("/pageerror");
+    }
+};
+
+const UnBlockRam = async (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log("This is unblocked id", id);
+        await Variant.findByIdAndUpdate(id,{$set:{isBlocked:false}});
+        res.redirect("/admin/getVariant");
+    } catch (error) {
+        console.error("Error unblocking RAM:", error);
+        res.redirect("/pageerror");
+    }
+};
+
+const deleteVariant = async(req,res)=>{
+    try {
+        const id = req.params.id
+        await Variant.deleteOne({_id:id})
+        res.redirect("/admin/getVariant")
+        console.log('Delete',id)
+    } catch (error) {
+        
+    }
+}
+
+const blockedStorage = async(req,res)=>{
+    try {
+        const id = req.params.id
+        await Variant.findByIdAndUpdate(id,{$set:{isBlocked:true}})
+        res.redirect("/admin/getVariant")
+        console.log("blockedStorage", id)
+    } catch (error) {
+        console.error("Error from BlockStorage",error)
+        res.redirect('/pageerror')
+    }
+}
+
+const unBlockStorage = async(req,res)=>{
+    try {
+        const id = req.params.id
+
+        await Variant.findByIdAndUpdate(id,{$set:{isBlocked:false}}) 
+        res.redirect("/admin/getVariant")
+        console.log("unBlockStorage", id)
+    } catch (error) {
+        console.error("Error from unBlockStorage",error)
+        res.redirect('/pageerror')
+    }
+}
+
+const deleteStorage = async (req,res)=>{
+    try {
+        const id = req.params.id
+        if(!id){
+            res.redirect("/pageerror")
+        }
+        await Variant.deleteOne({_id:id})
+        res.redirect("/admin/getVariant")
+    } catch (error) {
+        console.error("Error from in deleteStorage",error)
         res.redirect("/pageerror")
     }
 }
@@ -47,5 +127,11 @@ const addVariants = async (req,res)=>{
 module.exports = {
     getVariantList,
     addVariants,
-    getAddVariant
+    getAddVariant,
+    blockRam,
+    UnBlockRam,
+    deleteVariant,
+    blockedStorage,
+    unBlockStorage,
+    deleteStorage
 }
